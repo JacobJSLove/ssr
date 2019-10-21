d8eea773b963d93944b96ae13b43bc30f1acfe86
Jacob <kang2@o2.pl>
2019-10-21 21:36:22 +0200

    New routing

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Home from './component/Home';
 import UsersList from './component/UsersList';
 
-export default () => {
-    return (
-        <div>
-			<Route exact path="/" component={Home} />
-			<Route path="/users" component={UsersList} />
-		</div>
-    )
-};
+// export default () => {
+//     return (
+//         <div>
+// 			<Route exact path="/" component={Home} />
+// 			<Route path="/users" component={UsersList} />
+// 		</div>
+//     )
+// };
+
+//	for react-router-config we need to use objects for ssr!!
+
+export default [{
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        path: '/users',
+        component: UsersList
+    }
+]
