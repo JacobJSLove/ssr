2f6a566697d7be19a1067337d43a7c501846506e
Jacob <kang2@o2.pl>
2019-10-21 22:00:14 +0200

    Revert "Revert "routes""
    This reverts commit 73c452f6ea9c6345e3987a49e4c2a0bded4cb061.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import render from './helpers/render';
 import createStore from './helpers/createStore';
 
@@ -10,6 +12,10 @@ const app = express();
 app.use(express.static('public'));
 app.get('*', (req, res) => {
     const store = createStore();
+
+    // Check current routes with routes from router
+    // what components we need at this path
+    matchRoutes(Routes, req.path);
 
     res.send(render(req, store));
 });
